2130f4a3a9de001bed3ad7c146861df2b69288a1
Wisulve-328 <440042792@gapps.yrdsb.ca>
2026-05-08 03:01:28 -0400

    update the code
    add a method for the label to calculate the times of the game.

--- MyWorld.java
+++ MyWorld.java
@@ -1,10 +1,15 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    public int score = 0;
+    Label scoreLabel;
     public MyWorld() {
         super(600, 400, 1);
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
+        
+        scoreLabel = new Label(0,80);
+        addObject(scoreLabel, 20, 30);
         
         createApple();
     }
@@ -13,5 +18,9 @@ public class MyWorld extends World {
         int x = Greenfoot.getRandomNumber(600);
         int y = Greenfoot.getRandomNumber(300);
         addObject(apple, x, y);
+    }
+    public void increaseScore(){
+        score++;
+        scoreLabel.setValue(score);
     }
 }
