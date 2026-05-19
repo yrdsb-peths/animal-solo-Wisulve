7841bdbf38be698a4834ff064c4f555ab26a718b
Wisulve-328 <440042792@gapps.yrdsb.ca>
2026-05-19 11:21:58 -0400

    update code

--- Apple.java
+++ Apple.java
@@ -15,10 +15,10 @@ public class Apple extends Actor
     public void act()
     {
         int x = getX();
-        int y = getY();
+        int y = getY() + 2;
         setLocation(x, y);
         MyWorld world = (MyWorld)getWorld();
-        if(getY() <= world.getHeight()){
+        if(getY() >= world.getHeight()){
             world.gameOver();
             world.removeObject(this);
         }
